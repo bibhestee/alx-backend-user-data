9a0bce9c5cc026495f039741528e12bf2ff62a04
Bibest <bibhestee@gmail.com>
2023-11-07 15:35:30 +0100

    decode base64 to utf-8 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic Authentication Module
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import binascii
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,22 @@ class BasicAuth(Auth):
         if not auth or type(auth) != str or not auth.startswith('Basic '):
             return None
         return auth.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+            decode base64 authorization header
+            Arguments:
+                base64_authorization_header
+            Return:
+                None if not base64 encoded
+        """
+        base64_auth_header = base64_authorization_header
+        if not base64_auth_header or type(base64_auth_header) != str:
+            return None
+        try:
+            decoded_base64_auth = b64decode(base64_auth_header)
+            return decoded_base64_auth.decode('utf-8')
+        except binascii.Error:
+            return None

d412d3b8d07a2ccb3904b0ddf64396a733630c4b
Bibest <bibhestee@gmail.com>
2023-11-07 14:06:26 +0100

    base64 extract method added to basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """
         BasicAuth - Basic Authentication Class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """
+            extract base64 auth header
+            Arguments:
+                authorization_header: authorization header
+            Return:
+                None
+        """
+        auth = authorization_header
+        if not auth or type(auth) != str or not auth.startswith('Basic '):
+            return None
+        return auth.split(' ')[1]

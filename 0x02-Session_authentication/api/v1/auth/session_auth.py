af841157f81c16f4489afb108261be82a4428ef7
Bibest <bibhestee@gmail.com>
2023-11-09 00:00:16 +0100

    get user instance based on cookie id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -37,3 +38,15 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+            current user
+            Arguments:
+                request: flask request method
+            Return:
+                User Instance or None
+        """
+        cookie_value = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie_value)
+        return User.get(user_id)

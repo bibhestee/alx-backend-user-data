356f5523a2a513202a5234b06e93f944e5fb96cb
Bibest <bibhestee@gmail.com>
2023-11-08 23:01:16 +0100

    create session and return session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,25 @@
     Session Authentication Module
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
         SessionAuth - Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            create session
+            Arguments:
+                user_id: the user id
+            Return:
+                Session ID or None
+        """
+        if not user_id and type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

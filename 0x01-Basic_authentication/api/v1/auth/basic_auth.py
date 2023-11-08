5ab794eb19dfa96abc7caf63fbed1afdc58b3934
Bibest <bibhestee@gmail.com>
2023-11-08 14:17:53 +0100

    Search user with credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,9 @@ Basic Authentication Module
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 import binascii
+from typing import TypeVar
+from models.user import User
+user = User()
 
 
 class BasicAuth(Auth):
@@ -61,3 +64,36 @@ class BasicAuth(Auth):
             return (None, None)
         details = decoded_base64_authorization_header.split(':')
         return (details[0], details[1])
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+            user object from credentials
+            Arguments:
+                user_email: user email address
+                user_pwd: user password
+            Return:
+                User instance or None
+        """
+        if not user_email or type(user_email) != str:
+            return None
+        if not user_pwd or type(user_pwd) != str:
+            return None
+        user.load_from_file()
+        users = user.all()
+        if users != []:
+            match_user = [user for user in users if user.email ==
+                          user_email]
+            # Check if user is in database
+            if match_user == []:
+                # Use the search method is no user in DB
+                obj = user.search({'email': user_email})
+                if obj != []:
+                    # Validate password
+                    if obj[0].is_valid_password(user_pwd):
+                        return obj[0]
+            else:
+                # Validate password
+                if match_user[0].is_valid_password(user_pwd):
+                    return match_user[0]
+        return None

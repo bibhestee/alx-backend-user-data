5b2d673c2932fd838190af8f8f69370745447e6b
Bibest <bibhestee@gmail.com>
2023-11-09 09:26:15 +0100

    add expiration session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -22,6 +23,8 @@ if auth:
         auth = BasicAuth()
     elif auth == 'session_auth':
         auth = SessionAuth()
+    elif auth == 'session_exp_auth':
+        auth = SessionExpAuth()
     else:
         auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+    Session Authentication Expiration Module
+"""
+from api.v1.auth.session_auth import SessionAuth
+from uuid import uuid4
+from os import getenv
+from models.user import User
+from datetime import timedelta, datetime
+
+
+class SessionExpAuth(SessionAuth):
+    """
+        SessionExpAuth - Session Authentication Expiration
+    """
+    user_id_by_session_id = {}
+
+    def __init__(self):
+        """ initialize """
+        super().__init__()
+        session_duration = getenv('SESSION_DURATION')
+        if not session_duration or int(session_duration):
+            self.session_duration = 0
+        self.session_duration = int(session_duration)
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            create session
+            Arguments:
+                user_id: the user id
+            Return:
+                Session ID or None
+        """
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        session_dict = {'user_id': user_id, 'created_at': datetime.now()}
+        self.user_id_by_session_id[session_id] = session_dict
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+            user_id_for_session_id
+            Arguments:
+                session_id: session id
+            Return:
+                user id
+        """
+        if not session_id:
+            return None
+        user_dict = self.user_id_by_session_id.get(session_id)
+        if not user_dict:
+            return None
+        if self.session_duration <= 0:
+            return user_dict.get('user_id')
+        if not user_dict.get('created_at'):
+            return None
+        duration = timedelta(seconds=self.session_duration)
+        exp = user_dict.get('created_at') + duration
+        if exp < datetime.now():
+            return None
+        return user_dict.get('user_id')

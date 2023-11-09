3cf0253f106ed7ea4508278f5104c9f6cdf307a2
Bibest <bibhestee@gmail.com>
2023-11-09 12:51:31 +0100

    db cookie session and exp

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,7 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
-
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,6 +25,8 @@ if auth:
         auth = SessionAuth()
     elif auth == 'session_exp_auth':
         auth = SessionExpAuth()
+    elif auth == 'session_db_auth':
+        auth = SessionDBAuth()
     else:
         auth = Auth()
 

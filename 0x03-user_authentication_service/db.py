12fa99be03a6d7d6b8ee1a2b715c23a761d72236
Bibest <bibhestee@gmail.com>
2023-11-14 16:02:02 +0100

    modified update user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,7 +57,7 @@ class DB:
                     setattr(user, k, v)
                 else:
                     raise ValueError
-        except Exception:
+        except NoResultFound:
             raise ValueError
         self._session.commit()
         return None
